678f5c1face0e377313fdb7a66f54f885f3e22bf
Unknown <lex.punter@gmail.com>
2018-10-22 13:35:25 +0200

    Added api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const css = require('./app.scss');
 import 'jquery'
 
+class BoggleBoard {
+    constructor(id, board){
+        this.id = id
+        this.letters = board
+    }
+}
+
 class Game{
-    constructor(score, board, name){
+    constructor(score, boggleBoardId, name){
         this.score = score
-        this.board = board
+        this.boggleBoardId = boggleBoardId
         this.name = name
     }
 }
@@ -14,41 +21,13 @@ var inputName = $('#inputName'),
  endScreen = $('#endScreen'),
  startScreen = $('#startScreen'),
  games = [];
-
 $(document).ready(() => {
     inputName.focus();
 })
 
-var dice = [
-    ['R', 'I', 'F', 'O', 'B', 'X'],
-    ['I', 'F', 'E', 'H', 'E', 'Y'],
-    ['D', 'E', 'N', 'O', 'W', 'S'],
-    ['U', 'T', 'O', 'K', 'N', 'D'],
-    ['H', 'M', 'S', 'R', 'A', 'O'],
-    ['L', 'U', 'P', 'E', 'T', 'S'],
-    ['A', 'C', 'I', 'T', 'O', 'A'],
-    ['Y', 'L', 'G', 'K', 'U', 'E'],
-    ['Q', 'B', 'M', 'J', 'O', 'A'],
-    ['E', 'H', 'I', 'S', 'P', 'N'],
-    ['V', 'E', 'T', 'I', 'G', 'N'],
-    ['B', 'A', 'L', 'I', 'Y', 'T'],
-    ['E', 'Z', 'A', 'V', 'N', 'D'],
-    ['R', 'A', 'L', 'E', 'S', 'C'],
-    ['U', 'W', 'I', 'L', 'R', 'G'],
-    ['P', 'A', 'C', 'E', 'M', 'D']
-];
-
 var letters = $('.boggle'),
  word = "", game, isGameEnd, timerStart;
 const board = $('#board');
-
-function acsendingSort(a, b)
-    {
-        // if they are equal, return 0 (no sorting)
-        if (a.score == b.score) { return 0; }
-        // return 1 if b is greater than a else return 0
-        return a.score < b.score ? 1 : 0;
-    }
 
 function addLetter(letter){
     $('.wordBlock').append(letter);
@@ -95,38 +74,27 @@ function updateBoard(col, row){
     }
 }
 
-function create(name = "", customBoard) {
+function create(name = "", boggleBoard) {
+    console.log(boggleBoard)
     timerStart = new Date;
     $('#gameField').show();
     endScreen.hide();
     startScreen.hide();
     $('#scoreTable').find("tr:gt(0)").remove();
-
     board.empty();
-    game = new Game(0, [], name);
+        isGameEnd = false;
+
+    game = new Game(0, boggleBoard.id, name);
     for (let row = 0; row < 4; row++){
         board.append("<tr>");
         for (let col = 0; col < 4; col++)
         {
-            // get the dice number to use
-            let diceNumber = (row * 4) + col;
-            let letter = ''
-            if (typeof customBoard === 'undefined'){
-
-                // get a random number between 0 - 6 form the letter on the dice
-                let diceLetter = Math.floor(Math.random() * 6)
-                letter = dice[diceNumber][diceLetter];
-            } else {
-                letter = customBoard[diceNumber]
-            }
-
-            game.board.push(letter);
+            let letter = boggleBoard.letters[row][col]
             board.append("<td data-col-index=" + col + " data-row-index=" + row + " class=boggle>" + letter + "</td>");
-
         }
         board.append("</tr>");
-        isGameEnd = false;
     }
+
     letters = $('.boggle');
 
     $(letters).on('click', event => {
@@ -145,36 +113,41 @@ function create(name = "", customBoard) {
 function endGame(){
     $('#gameField').hide();
     isGameEnd = true;
-    games.push(game);
-    games.sort(acsendingSort);
-    $('#score').text('Score: ' + game.score);
-    let highScoreTable = $('#highScoreTable');
-    highScoreTable.find("tr:gt(0)").remove();
-    for (let g = 0; g < games.length; g++){
-        highScoreTable.append("<tr><td>" + games[g].name + "</td> <td>" + games[g].score + 
-                                "</td> <td><button data-game-index="+g+" class=playBoardButton>Play this board</button></td></tr>")
-    }
-    clear();
-}
 
-function calcWordScore(word){
-    let length = String(word).length;
-    switch(length){
-        case 3:
-        return 1;
-        case 4:
-        return 1;
-        case 5:
-        return 2;
-        case 6:
-        return 3;
-        case 7:
-        return 5
-    }
-    if (length > 7)
+    console.log(JSON.stringify(game))
+    $.ajax({
+        url : '/api/Boggle',
+        type : 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(game)
+    });
+
+
+    $.ajax({
+
+        url : '/api/Boggle/GetHighScore',
+        type : 'GET',
+        dataType: 'json',
+        success : function(data) {  
+            console.log(data)            
+            games = data
+            console.log(games)
+
+            $('#score').text('Score: ' + game.score);
+            let highScoreTable = $('#highScoreTable');
+            highScoreTable.find("tr:gt(0)").remove();
+            for (let g = 0; g < games.length; g++){
+                highScoreTable.append("<tr><td>" + games[g].Name + "</td> <td>" + games[g].Score + 
+                                        "</td> <td><button data-game-index="+ games[g].BoggleBoardId +" class=playBoardButton>Play this board</button></td></tr>")
+            }
+            clear();
+    },
+    error : function(request,error)
     {
-        return 11;
+        alert("Request: "+JSON.stringify(error));
     }
+});
+
 }
 
 $('#addWordButton').on('click', () => {
@@ -182,25 +155,80 @@ $('#addWordButton').on('click', () => {
     {
         return;
     }
-    let score = calcWordScore(word);
-    game.score += score;
-    $('#scoreTable').append("<tr><td>" + word + "</td> <td>" + score + "</td></tr>");
-    clear();
+
+    
+    $.ajax({
+
+        url : '/api/Boggle/ScoreWord',
+        type : 'GET',
+        dataType: 'json',
+        data: {
+            word: word
+        },
+        success : function(data) {    
+            let score = parseInt(data)
+            if (score > 0){
+                game.score += score;
+                $('#scoreTable').append("<tr><td>" + word + "</td> <td>" + score + "</td></tr>");
+                clear();
+            }         
+        },
+        error : function(request,error)
+        {
+            alert("Request: "+JSON.stringify(error));
+        }
+    });
 })
 
 $('#clearButton').on('click', () =>{
     clear();
 })
 
-$('#startButton').on('click', () =>{
+startButton.on('click', () =>{
     let name = $('#inputName').val();
-    create(name);
+
+    $.getJSON('/api/Boggle', data => {
+        create(name, new BoggleBoard(data.Id, data.Letters));
+    })
+
+    // $.ajax({
+
+    //     url : '/api/Boggle/',
+    //     type : 'GET',
+    //     dataType: 'json',
+    //     success : function(data) {  
+    //         console.log(data.Letters)
+    //         //return new BoggleBoard(data.Id, data.Letters)
+    //     },
+    //     error : function(request,error)
+    //     {
+    //         alert("Request: "+JSON.stringify(error));
+    //     }
+    // });
+    // create(name, getBoard())
 })
 
 $('#highScoreTable').on('click', '.playBoardButton', event => {
-    console.log('play borad')
-    let gameIndex = parseInt(event.currentTarget.getAttribute('data-game-index'));
-    create(game.name, games[gameIndex].board);
+
+    let gameIndex = event.currentTarget.getAttribute('data-game-index');
+    console.log(gameIndex)
+    $.ajax({
+
+        url : '/api/Boggle/GetBoggleBoardById',
+        type : 'GET',
+        dataType: 'json',
+        data: {
+            boardId: gameIndex
+        },
+        success : function(data) {  
+            console.log(data.Letters)
+            create(name, new BoggleBoard(data.Id, data.Letters));
+        },
+        error : function(request,error)
+        {
+            alert("Request: "+JSON.stringify(error));
+        }
+    });
 })
 
 setInterval(function() {
@@ -232,3 +260,38 @@ $('#homeButton').on('click', () => {
     endScreen.hide();
     startScreen.show();
 })
+
+function getBoard()
+{
+    $.ajax({
+
+        url : '/api/Boggle/',
+        type : 'GET',
+        dataType: 'json',
+        async: 'false',
+        success : function(data) {  
+            console.log(data.Letters)
+            //return new BoggleBoard(data.Id, data.Letters)
+            create(name, boggleBoard);
+        },
+        error : function(request,error)
+        {
+            alert("Request: "+JSON.stringify(error));
+        }
+    });
+}
+
+
+// $.ajax({
+
+//     url : '/api/Boggle',
+//     type : 'GET',
+//     dataType: 'json',
+//     success : function(data) {              
+//         alert('Data: '+ data);
+//     },
+//     error : function(request,error)
+//     {
+//         alert("Request: "+JSON.stringify(error));
+//     }
+// });
